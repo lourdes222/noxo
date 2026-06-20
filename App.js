584a93cb782c59aa6228d29a2a8c98093f2e4cf3
lourdes222 <lourdes.munoz66622@gmail.com>
2026-06-19 22:15:23 -0300

    19/06 navegacion bottom

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'; 
 
 import HomeView from './src/vistas/HomeView';
 import ConfigScreen from './src/vistas/ConfigScreen';
+import LoginScreen from './src/vistas/Inicioses';
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator(); 
+const Tab = createBottomTabNavigator();
+
+function MainTabs(){
+  return(
+    <Tab.Navigator
+    screenOptions={({route})=>({
+      tabBarIcon: ({focused, color, size})=>{
+        let iconName;
+        if (route.name==='Home'){
+          iconName= focused? 'home':'home-outline';
+        }
+        else if(route.name==='LoginTab'){
+          iconName=focused? 'log-in': 'log-in-outline';
+        }
+        else if(route.name==='ProfileTab'){
+          iconName=focused?'person': 'person-outline'
+        }
+        return <Ionicons name={iconName} size={size} color={color}/>;
+      },
+      tabBarActiveTintColor:'#55E6C1',
+      tabBarInactiveTintColor: 'gray',
+      tabBarStyle:{
+        backgroundColor: '#1E292E',
+        borderTopColor: '#3A5A63',
+        heigth: 60,
+        paddingBottom: 8,
+      },
+      headerShown: false,
+    })}>
+      <Tab.Screen name="Home" component={HomeView} options={{title: 'Inicio'}}/>
+      <Tab.SCreen name="LoginTab" component={LoginScreen} options={{title: 'Login'}}/>
+      <Tab.Screen name="ProfileTab" component={ProfileScreen} options={{title: 'Perfil'}}/>
+    </Tab.Navigator>
+  );
+}
 
 function ProfileScreen() {
   return (
@@ -23,27 +61,28 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator
         screenOptions={{
-          drawerActiveTintColor: '#2f95dc',
+          drawerActiveTintColor: '#55E6C1',
           drawerInactiveTintColor: 'gray',
-        }}
-      >
+          drawerStyle:{
+            backgroundColor: '#1E292E',
+          },
+          headerStyle:{
+            backgroundColor: '#1E292E',
+          },
+          headerTintColor: '#55E6C1'
+        }}>
         <Drawer.Screen
-          name="Home"
-          component={HomeView}
-          options={{ title: 'Inicio' }}
+          name="Main"
+          component={MainTabs}
+          options={{ title: 'Inicio(Tabs)'}}
         />
         <Drawer.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: 'Perfil' }}
-        />
-        <Drawer.Screen
-          name="Settings"
+          name="Setttings"
           component={ConfigScreen}
-          options={{ title: 'Configuración' }}
+          options={{ title: 'Configuración'}}
         />
       </Drawer.Navigator>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </NavigationContainer>
   );
 }
@@ -54,11 +93,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#1E292E',
   },
   title: {
     fontSize: 24,
     fontWeight: '700',
     marginBottom: 10,
+    color: '#FFFFFF',
   },
+  texto:{
+    color: '#FFFFF'
+  }
 });
--- src/vistas/Inicioses.js
+++ src/vistas/Inicioses.js
@@ -13,5 +13,35 @@ const LoginScreen=()=>{
     );
 };
 const styles= StyleSheet.create({
-    container
-})
+    container:{
+        flex: 1,
+        backgroundColor: '#1E292E',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    tarjeta:{
+        backgroundColor: '#3A5A63',
+        padding: 40,
+        borderRadius: 30,
+        width: '85%',
+        alignItems: 'center',
+    },
+    tituloTarjeta:{
+        color: '#FFFFFF',
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 30,
+    },
+    boton:{
+        backgroundColor: '#55E6C1',
+        paddingVertical: 12,
+        paddingHorizontal: 40,
+        borderRadius: 25,
+    },
+    textoBoton:{
+        color: '#1E292E',
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
+export default LoginScreen;
